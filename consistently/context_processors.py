4df32024450949b28d56460e99797ebc62ae1a98
jamstooks <ben.stookey@gmail.com>
2018-10-07 21:43:08 +0000

    added github avatar

--- consistently/context_processors.py
+++ consistently/context_processors.py
@@ -10,3 +10,14 @@ def react_vars(request):
         'main_js': settings.REACT_JS_PATH,
         'main_css': settings.REACT_CSS_PATH
     }
+
+
+def github_avatar(request):
+
+    if request.user.is_authenticated:
+        base_url = "https://avatars0.githubusercontent.com/u/%s?s=40&v=4"
+        github = request.user.social_auth.get(provider='github')
+        return {
+            'avatar': base_url % github.uid
+        }
+    return {'avatar': None}
