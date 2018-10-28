a9bf46dfc868fa2e462eca6c5021846c0bb038fd
jamstooks <ben.stookey@gmail.com>
2018-10-28 15:15:44 +0000

    better menu closing experience

--- consistently/static/js/core.js
+++ consistently/static/js/core.js
@@ -1,3 +1,9 @@
-function toggleMenu(id) {
-    $("#" + id).toggleClass('hidden');
-}
+$(".menu-link").on('click', function(event) {
+    let target = this.getAttribute("data-target");
+    $("#" + target).toggleClass('hidden');
+    event.stopPropagation();
+});
+
+$(window).on('click', function() {
+    $(".menu").addClass('hidden');
+});
